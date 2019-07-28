9295d30e7f23004d89b6f1cd9333fcc0f21563a3
epaus <epaus@paushaus.net>
2019-07-28 13:01:32 -0500

    Initial Commit

---
+++ The Suggestion-ATOR MAX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Suggestion-ATOR MAX
+//
+//  Created by Estelle Paus on 7/28/19.
+//  Copyright © 2019 Paus Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
